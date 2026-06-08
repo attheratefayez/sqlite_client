1278296dab3b372f938d84bc609cfe10a5b57f4d
fayez <faizurrahman.fayez@gmail.com>
2026-06-08 16:33:04 +0600

    fix: TableTab splitter 35-65 ratio

--- ui/data_browser.py
+++ ui/data_browser.py
@@ -560,9 +560,9 @@ class TableTab(QWidget):
         self._browser = DataBrowser(worker, table_name, columns, total_count)
         splitter.addWidget(self._browser)
 
-        splitter.setSizes([400, 600])
-        splitter.setStretchFactor(0, 40)
-        splitter.setStretchFactor(1, 60)
+        splitter.setSizes([350, 650])
+        splitter.setStretchFactor(0, 35)
+        splitter.setStretchFactor(1, 65)
         layout.addWidget(splitter)
 
     def refresh(self) -> None:
